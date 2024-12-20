45ee9419d27fb12638d216af955db1a33ed2827c
Samuel Moura <samuelmalves1@gmail.com>
2024-12-19 23:33:47 -0300

    seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,10 +4,12 @@ import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +17,10 @@ public class Main {
 
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com",
+                LocalDateTime.now(), 3000.0, department);
+        System.out.println(seller);
 
         DB.closeConnection();
 
